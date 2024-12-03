d3222ad3fef9746498635fb52c9cc6a53b4a4c74
Nadira Ahmadi <73304010+nadiahmdi@users.noreply.github.com>
2024-12-03 23:00:48 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    const progressBars = document.querySelectorAll('.progress');
+    progressBars.forEach(bar => {
+        const width = bar.style.width;
+        bar.style.width = '0';
+        setTimeout(() => {
+            bar.style.width = width;
+        }, 500);
+    });
+});
